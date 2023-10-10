738dbdba19ebf9f4ef43c296fef2b4adbbce7f6c
thoriqafa <thoriqafa5@gmail.com>
2023-10-10 13:58:42 +0700

    Update for Website View

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,9 +9,25 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Wisata Badung'),
+        title: Text('Wisata Badung. Size: ${MediaQuery.of(context).size.width}'),
       ),
-      body: ListView.builder(
+      body: LayoutBuilder(builder: (BuildContext context, BoxConstraints constraints){
+        if (constraints.maxWidth <= 600) {
+          return TourismPlaceList();
+        } else {
+          return TourismPlaceGrid();
+        }
+      })
+    );
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return ListView.builder(
         itemBuilder: (context, index) {
           final TourismPlace place = tourismPlaceList[index];
           return InkWell(
@@ -25,7 +41,7 @@ class MainScreen extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Expanded(
-                    flex: 1,
+                      flex: 1,
                       child: Image.asset(place.imageAsset)
                   ),
                   Expanded(
@@ -54,6 +70,53 @@ class MainScreen extends StatelessWidget {
           );
         },
         itemCount: tourismPlaceList.length,
+      );
+    }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  const TourismPlaceGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: 4,
+        children: tourismPlaceList.map((place) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return DetailScreen(place: place);
+              }));
+            },
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(child: Image.asset(place.imageAsset, fit: BoxFit.cover,),),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                        place.name,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 8, bottom: 8),
+                    child: Text(
+                      place.location,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
